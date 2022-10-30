629e7895f6eb10713a90bccd35b502ae7dc5e8a7
Andre Luiz <aluiz815@gmail.com>
2022-10-30 14:58:57 -0300

    Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,24 @@
+import Logo from '../../assets/logo.svg'
+import {
+  HeaderContainer,
+  ActionsContainer,
+  LocationContainer,
+  CardContainer,
+} from './style'
+import { ShoppingCart, MapPin } from 'phosphor-react'
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="" />
+      <ActionsContainer>
+        <LocationContainer>
+          <MapPin size={22} weight="fill" />
+          <span>Recife, PE</span>
+        </LocationContainer>
+        <CardContainer>
+          <ShoppingCart size={22} weight="fill" />
+        </CardContainer>
+      </ActionsContainer>
+    </HeaderContainer>
+  )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 10rem;
+`
+export const ActionsContainer = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+export const LocationContainer = styled.div`
+  display: flex;
+  gap: 0.25rem;
+  justify-content: center;
+  align-items: center;
+  padding: 0.625rem 2.125rem;
+  background: ${(props) => props.theme['purple-light']};
+  border-radius: 6px;
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+
+  span {
+    font-size: 0.875rem;
+    font-family: 'Roboto', sans-serif;
+    color: ${(props) => props.theme['purple-dark']};
+  }
+`
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  background: ${(props) => props.theme['yellow-light']};
+  border-radius: 6px;
+  color: ${(props) => props.theme['yellow-dark']};
+  cursor: pointer;
+`
